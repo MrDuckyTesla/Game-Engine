736d375dee51e3462361254690e127a5dcb30e13
Nico L. <90070276+MrDuckyTesla@users.noreply.github.com>
2026-06-13 17:49:24 -0500

    vector

--- src/engine/util/neuralnet/Vector.java
+++ src/engine/util/neuralnet/Vector.java
@@ -1,6 +1,17 @@
 package engine.util.neuralnet;
 
+import engine.util.physics.Vector2D;
+
 public class Vector extends Matrix {
+	
+	public static float dot(Vector a, Vector b) {
+		if (a.getHgt() == b.getHgt()) {
+			float prod = 0;
+			for (int i = 0; i < a.getHgt(); i++) {
+				prod += a.get(i)*b.get(i);
+			} return prod;
+		} return Float.NaN;
+	}
 	
 	public Vector(float[] v) {
 		super(1, v.length);
@@ -11,12 +22,16 @@ public class Vector extends Matrix {
 		super(1, hgt);
 	}
 	
-	public float dot(Vector other) {
-		return -1;
+	public Vector2D getNormal() {
+//		float hypot = x*x + y*y;
+//		if (hypot == 0) {return new Vector2D();}
+//		float overH = 1 / (float)Math.sqrt(hypot);
+//		return new Vector2D(x*overH, y*overH);
+		return null;
 	}
 	
-	public void normalize() {
-		
+	public float get(int num) {
+		return this.getMatrix()[num];
 	}
 
 }
